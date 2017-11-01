373cdd782feb94663d44ee1bbfa88222443c5839
samuelssnider <sam_redins@yahoo.com>
2017-11-01 11:19:05 -0600

    most factored out of the controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,15 +1,10 @@
 class SearchController < ApplicationController
   
   def index
+    zip = params["q"]
     # @conn = Faraday.new(url: "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=phone,storeType,name,city,distance&pageSize=17&apiKey=#{ENV['best_buy_key']}")
-    raw_stores = StoreService.new.parse_url(params[:q]) #JSON.parse(@conn.get.body, symbolize_names: true)
-    @count = raw_stores[:total]
-    @stores = raw_stores[:stores].map do |raw_store|
-      Store.new(raw_store[:name],
-                raw_store[:city],
-                raw_store[:distance],
-                raw_store[:phone],
-                raw_store[:storeType])
-    end
+    service = StoreService.new #JSON.parse(@conn.get.body, symbolize_names: true)
+    @count = service.get_stores_count(zip)
+    @stores = service.get_stores(zip)
   end
 end
--- app/services/store_service.rb
+++ app/services/store_service.rb
@@ -1,4 +1,5 @@
 class StoreService
+  attr_reader :get_stores_count, :get_stores
   def parse_url(zip)
     conn = Faraday.new(url: "https://api.bestbuy.com/") do |faraday| #?format=json&show=phone,storeType,name,city,distance&pageSize=17&apiKey=#{ENV['best_buy_key']}") do |faraday|
       faraday.adapter Faraday.default_adapter
@@ -14,5 +15,21 @@ class StoreService
       apiKey: ENV['best_buy_key'],
       format: 'json'
     }
+  end
+  
+  def get_stores(zip)
+    raw_stores = self.parse_url(zip)
+    raw_stores[:stores].map do |raw_store|
+      Store.new(raw_store[:name],
+                raw_store[:city],
+                raw_store[:distance],
+                raw_store[:phone],
+                raw_store[:storeType])
+    end
+  end
+  
+  def get_stores_count(zip)
+    raw_stores = self.parse_url(zip)
+    raw_stores[:total]
   end
 end
