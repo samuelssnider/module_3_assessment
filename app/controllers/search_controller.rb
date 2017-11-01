e9c9643c4ea0f0a2f6516a524c6190f22c4991fa
samuelssnider <sam_redins@yahoo.com>
2017-11-01 09:47:25 -0600

    working on store model and view

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,15 @@
 class SearchController < ApplicationController
   
   def index
+    @conn = Faraday.new(url: "https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=phone,storeType,name,city,distance&pageSize=17&apiKey=#{ENV['best_buy_key']}")
+    raw_stores = JSON.parse(@conn.get.body, symbolize_names: true)
+    @count = raw_stores[:total]
+    raw_stores[:stores].each do |raw_store|
+      Store.new(raw_store[:name],
+                raw_store[:city],
+                raw_store[:distance],
+                raw_store[:phone],
+                raw_store[:storeType])
+    end
   end
 end
---
+++ app/services/store.rb
@@ -0,0 +1,9 @@
+class Store
+  def initialize(name, city, distance, phone, type)
+    @name = name
+    @city = city
+    @distance = distance
+    @phone = phone
+    @type = type
+  end
+end
